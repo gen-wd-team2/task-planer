57ec086e3244579b2ce7d040e5cb8735603b6e1f
Kingsford Owusu-Achiaw <kingsfordowusu6@gmail.com>
2024-06-10 13:04:57 +0000

    Background style added

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // javascript for index.html
-const container = document.querySelector('.tasks');
+const container = document.querySelector(".tasks");
 // const searchForm = document.querySelector('.search');
 
 const renderTasks = async (term) => {
   console.log(term);
-  let uri = 'http://localhost:3000/tasks';
+  let uri = "http://localhost:3000/tasks";
 
   const res = await fetch(uri);
   const tasks = await res.json();
   console.log(tasks);
 
-  let template = '';
-  tasks.reverse().forEach(task => {
+  let template = "";
+  tasks.reverse().forEach((task) => {
     template += `
       <div class="card mb-3 shadow-lg rounded" data-id${task.id} contenteditable="true">
         <div class="card-body">
@@ -24,16 +24,14 @@ const renderTasks = async (term) => {
               <li class="list-group-item"><strong>Status</strong>: ${task.status}</li>
         </ul>
       </div>
-    `
+    `;
   });
 
-
   container.innerHTML = template;
-}
-
+};
 
 // javascript for create.html
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
 const createTask = async (e) => {
   e.preventDefault();
@@ -44,46 +42,48 @@ const createTask = async (e) => {
     assignedTo: form.assignedTo.value,
     dueDate: form.dueDate.value,
     status: form.status.value,
-  }
+  };
 
-  await fetch('http://localhost:3000/tasks', {
-    method: 'POST',
+  await fetch("http://localhost:3000/tasks", {
+    method: "POST",
     body: JSON.stringify(task),
-    headers: { 'Content-Type': 'application/json' }
-  })
+    headers: { "Content-Type": "application/json" },
+  });
 
-  window.location.replace('/')
-}
-
+  window.location.replace("/");
+};
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-  'use strict'
+  "use strict";
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+  const forms = document.querySelectorAll(".needs-validation");
 
   // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }else {
-        createTask(event);
-      }
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          createTask(event);
+        }
 
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
 
 // form.addEventListener('submit', createTask);
 // search with keyword or name
-container.innerHTML = template;
 
-searchInput.addEventListener('input', (e) => {
+searchInput.addEventListener("input", (e) => {
   renderTasks(e.target.value);
 });
 
-window.addEventListener('DOMContentLoaded', () => renderTasks());
+window.addEventListener("DOMContentLoaded", () => renderTasks());
